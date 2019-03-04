a472ee9125b77ba753562842027accb944b6115b
Vladimir Masarik <vmasarik@redhat.com>
2019-03-04 11:36:01 +0100

    added automatic repair of cluster for 3.11

--- start/init-vm.sh
+++ start/init-vm.sh
@@ -1,9 +1,16 @@
 oc project openshift-logging
 
-mkdir secret && cd secret
+mkdir /data/src/start/secret && cd /data/src/start/secret
 
 oc extract secrets/logging-elasticsearch
 
 cd ..
 
-docker build --label rallylabel -t docker.io/rallytag .
+docker build -t rallyperf .
+
+oc set image ds logging-fluentd fluentd-elasticsearch=openshift/origin-logging-fluentd
+oc set image $(oc get dc -o name | grep es) elasticsearch=openshift/origin-logging-elasticsearch5
+oc rollout cancel $(oc get dc -o name | grep es)
+oc rollout latest $(oc get dc -o name | grep es)
+oc set image deploymentconfig.apps.openshift.io/logging-kibana kibana=openshift/origin-logging-kibana5
+oc delete po logging-kibana-1-deploy
