b50a83da8067ea065b16d276483f4a7db1629b1f
Vladimir Masarik <vl631mas@htwg-konstanz.de>
2019-05-24 13:46:41 +0200

    refactored metadata adder script

--- py-scripts/label-for-bulk.py
+++ py-scripts/label-for-bulk.py
@@ -8,27 +8,12 @@ INDEX_LAST=31
 
 
 def main():
-    i = 0
     with open("logs1400k.json", "rt", encoding="UTF-8") as f:
         for line in f:
-            i += 1
-            
             
             tup = line.strip()
             
             meta = "{{\"index\": {{\"_index\": \"nasa-{:03d}\", \"_type\": \"docs\"}}}}".format(random.randint(INDEX_FIRST, INDEX_LAST))
-            #meta = "{{\"index\": {{\"_index\": \"nasa-{:03d}\", \"_type\": \"docs\"}}}}".format(0)
-
-
-            # x = random.randint(1,3)
-            # if x == 1:
-            #     meta = "{{\"index\": {{\"_index\": \"nasaone\", \"_type\": \"docs\", \"_id\": \"{}\"}}}}".format(i)
-            # elif x == 2:
-            #     meta = "{{\"index\": {{\"_index\": \"nasatwo\", \"_type\": \"docs\", \"_id\": \"{}\"}}}}".format(i)
-            # else:
-            #     meta = "{{\"index\": {{\"_index\": \"nasatre\", \"_type\": \"docs\", \"_id\": \"{}\"}}}}".format(i)
-
-
 
             res = "{}\n{}\n".format(meta,tup)
             print(res, end="")
